ca6a3c8cd5a1a8966fee03d9149b0e23623392a3
Carolina Mazzaglia <carolinamazzaglia@mac-mini-de-carolina.local>
2017-03-16 15:55:09 -0500

    Initial Commit

---
+++ Proyecto Modulo3 IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Proyecto Modulo3 IOS
+//
+//  Created by Carolina Mazzaglia on 16/03/17.
+//  Copyright © 2017 Carolina Mazzaglia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
